7ead3e3ede38c5ac3eea35501cf6fa1797806327
srlee <silleruss@gmail.com>
2020-03-01 15:00:29 +0900

    webview added function

--- app/src/main/java/com/xeun/kdol/MainActivity.kt
+++ app/src/main/java/com/xeun/kdol/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.xeun.kdol
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -12,16 +16,58 @@ class MainActivity : AppCompatActivity () {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val myWebView: WebView = findViewById(R.id.webView)
-        myWebView.settings.javaScriptEnabled = true
+        val main_webView: WebView = findViewById(R.id.activity_main_webView)
+        main_webView.settings.javaScriptEnabled = true
 
-        myWebView.webViewClient = object : WebViewClient() {
+        main_webView.settings.domStorageEnabled = true
+
+        main_webView.settings.userAgentString =
+            main_webView.settings.userAgentString.replace("; wv","")
+
+        main_webView.webViewClient = object : WebViewClient() {
             override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
                 view?.loadUrl(url)
                 return true
             }
         }
 
-        webView.loadUrl("https://srlee-temp.herokuapp.com/")
+        activity_main_webView.loadUrl("http://watori.jp/")
+    }
+
+    fun sendMessage(view : View) {
+        val builder = AlertDialog.Builder(this@MainActivity)
+        // Set the alert dialog title
+        builder.setTitle("App background color")
+
+        // Display a message on alert dialog
+        builder.setMessage("Are you want to set the app background color to RED?")
+
+        // Set a positive button and its click listener on alert dialog
+        builder.setPositiveButton("YES"){dialog, which ->
+            // Do something when user press the positive button
+            Toast.makeText(applicationContext,"Ok, we change the app background.",Toast.LENGTH_SHORT).show()
+
+            // Change the app background color
+            activity_main_webView.setBackgroundColor(Color.RED)
+        }
+
+
+        // Display a negative button on alert dialog
+        builder.setNegativeButton("No"){dialog,which ->
+            Toast.makeText(applicationContext,"You are not agree.",Toast.LENGTH_SHORT).show()
+            activity_main_webView.setBackgroundColor(Color.WHITE);
+        }
+
+
+        // Display a neutral button on alert dialog
+        builder.setNeutralButton("Cancel"){_,_ ->
+            Toast.makeText(applicationContext,"You cancelled the dialog.",Toast.LENGTH_SHORT).show()
+        }
+
+        // Finally, make the alert dialog using builder
+        val dialog: AlertDialog = builder.create()
+
+        // Display the alert dialog on app interface
+        dialog.show()
     }
 }
